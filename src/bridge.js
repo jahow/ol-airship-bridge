5c34998ff1317f43dd4ca8b2897684ff7d5ff90c
Olivier Guyot <olivier.guyot@camptocamp.com>
2019-12-17 13:40:53 +0100

    add animation in time widget series

--- src/bridge.js
+++ src/bridge.js
@@ -1,11 +1,13 @@
 import { generateBuckets } from './aggregation'
 import { debounce } from 'throttle-debounce'
 
+const ANIM_SPEED = 0.1 // full cycle by second
+
 /**
  *
  * @param {Map} map
  * @param {VectorSource} source
- * @param {HistogramWidget} widget
+ * @param {TimeSeriesWidget} widget
  * @param {string} attributeName
  * @param {function(min, max):void} updateSelection
  */
@@ -28,15 +30,60 @@ export function bindMapToTimeWidget(
   // bind view to time widget
   map.getView().on(['change:center', 'change:resolution'], throttledUpdate)
 
+  let playing = false
+  let playProgress = 0
+  let currentSelection = null
+
   // bind time widget to layer style
-  widget.addEventListener('selectionInput', evt => {
-    const selection = evt.detail === null ? null : evt.detail.selection
-    if (selection !== null) {
-      updateSelection(selection[0], selection[1])
-    } else {
-      updateSelection(null, null)
+  widget.addEventListener('selectionInput', event => {
+    currentSelection = event.detail === null ? null : event.detail.selection
+    if (!playing) {
+      if (currentSelection !== null) {
+        updateSelection(currentSelection[0], currentSelection[1])
+      } else {
+        updateSelection(null, null)
+      }
     }
   })
+  widget.addEventListener('seek', event => {
+    playProgress = event.detail / 100
+  })
+  widget.addEventListener('play', event => {
+    playing = true
+  })
+  widget.addEventListener('pause', event => {
+    playing = false
+  })
+
+  let lastUpdate = null
+  function updateAnimation() {
+    widget.progress = playProgress * 100
+    widget.playing = playing
+
+    if (playing) {
+      if (!lastUpdate) lastUpdate = Date.now()
+      else {
+        const now = Date.now()
+        playProgress += (now - lastUpdate) * 0.001 * ANIM_SPEED
+        playProgress = playProgress % 1
+        lastUpdate = now
+
+        const selection = currentSelection || [
+          widget.backgroundData[0].start,
+          widget.backgroundData[widget.backgroundData.length - 1].end,
+        ]
+        updateSelection(
+          selection[0],
+          selection[0] + playProgress * (selection[1] - selection[0])
+        )
+      }
+    } else {
+      lastUpdate = null
+    }
+
+    window.requestAnimationFrame(updateAnimation)
+  }
+  updateAnimation()
 
   return updateTimeWidget
 }
